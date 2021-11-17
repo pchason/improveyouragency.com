3017c0c472a15c29a96b3f20b451f4d57522a533
Paul Chason <paulchason@gmail.com>
2021-11-17 11:02:47 -0500

    Created new landing page content and template.

---
+++ src/content/pages/build-systems-for-growth.md
@@ -0,0 +1,9 @@
+---
+template: build-systems-for-growth-page
+slug: /build-systems-for-growth
+title: Build Digital Agency Systems for Growth
+---
+
+This is a test.
+
+Just send us a message using the form below or you can send us a DM on [Twitter](https://twitter.com/stackrole)
---
+++ src/templates/build-systems-for-growth-page.js
@@ -0,0 +1,95 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { graphql } from "gatsby"
+import { RiSendPlane2Line } from "react-icons/ri"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+export const pageQuery = graphql`
+  query BuildSystemsForGrowthQuery($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      excerpt(pruneLength: 140)
+      frontmatter {
+        title
+      }
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+const BuildSystemsForGrowth = ({ data }) => {
+  const { markdownRemark, site } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+
+  return (
+    <Layout className="contact-page" sx={contactStyles.contactPage}>
+      <Seo
+        title={frontmatter.title}
+        description={frontmatter.title + " " + site.siteMetadata.title}
+      />
+      <div className="wrapper">
+        <h1>{frontmatter.title}</h1>
+        <div
+          className="description"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+        <form
+          className="contact-form"
+          action="/thanks"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="build-systems-for-growth" />
+          <p>
+            <label>
+              Email
+              <input type="email" name="email" required />
+            </label>
+          </p>
+          <p className="text-align-right">
+            <button
+              className="button"
+              sx={{
+                variant: "variants.button",
+              }}
+              type="submit"
+            >
+              Send Message{" "}
+              <span className="icon -right">
+                <RiSendPlane2Line />
+              </span>
+            </button>
+          </p>
+        </form>
+      </div>
+    </Layout>
+  )
+}
+
+export default BuildSystemsForGrowth
+
+const contactStyles = {
+  contactPage: {
+    input: {
+      border: "6px solid",
+      borderColor: "inputBorder",
+      bg: "inputBackground",
+      outline: "none",
+    },
+    textarea: {
+      border: "6px solid",
+      borderColor: "inputBorder",
+      bg: "inputBackground",
+      outline: "none",
+    },
+  },
+}
