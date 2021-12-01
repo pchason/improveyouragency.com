2c1b802bd43c6fc9aae8f35d06f31866a6510bb2
Paul Chason <paulchason@gmail.com>
2021-12-01 09:09:54 -0500

    Removed alert() that was breaking netlify build.

--- src/components/layout-no-nav.js
+++ src/components/layout-no-nav.js
@@ -1,28 +1,13 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 import { useColorMode } from "theme-ui"
-// import { useStaticQuery, graphql } from "gatsby"
 
 import "../assets/scss/style.scss"
 import Footer from "./footer"
 
-// const query = graphql`
-//   query LayoutNoNavQuery {
-//     site {
-//       siteMetadata {
-//         siteTitle: title
-//       }
-//     }
-//     siteSearchIndex {
-//       index
-//     }
-//   }
-// `
-
 const LayoutNoNav = ({ children, className, props }) => {
 
   const [colorMode, setColorMode] = useColorMode();
-  alert(colorMode);
   setColorMode("default");
 
   return (
